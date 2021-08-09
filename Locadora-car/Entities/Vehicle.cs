8e046946fa035e96cbce96d3649df605dedc350c
diegolins10 <diegofernandeslins@gmail.com>
2021-08-09 20:56:55 -0300

    Class vehicle

---
+++ Locadora-car/Entities/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Locadora_car.Entities
+{
+    /*
+     * Classe para representar um objeto veiculo
+     */
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
