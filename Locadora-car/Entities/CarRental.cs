3fd540c4785bd6cb52bc18f64d2770aa2f1e5237
diegolins10 <diegofernandeslins@gmail.com>
2021-08-09 20:57:55 -0300

    class carRental utilizando a composicao de objetos das entidades

---
+++ Locadora-car/Entities/CarRental.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Locadora_car.Entities
+{
+    /*
+     * Classe para representar o aluguel de carro
+     */
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public Invoice Invoice { get; set; } //composicao de objetos
+        public Vehicle Vehicle { get; set; }
+
+        /*
+         * Construtor
+         */
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+
+
+    }
+}
